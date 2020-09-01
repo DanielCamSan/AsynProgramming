397037f6de4521ae1a0b45406a4ae360e4a524fc
Daniel Camacho <daniel.camacho.santacruz.5c@gmail.com>
2020-09-01 16:36:28 -0400

    Creando programa

---
+++ AsynProgramming/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+namespace AsynProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string result;
+            var cities = new Dictionary<string, string>(){
+                {"UK", "London, Manchester, Birmingham"},
+                {"USA", "Chicago, New York, Washington"},
+                {"India", "Mumbai, New Delhi, Pune"}
+            };
+            cities.Add("France", "Paris, Ratatouille");
+        
+            if (cities.TryGetValue("France", out result))
+            {
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
